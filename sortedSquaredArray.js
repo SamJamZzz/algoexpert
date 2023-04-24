e98c4cfe2623ed4c6b0b5a80d5ec3088a18dfb23
Sam Julian <samjulian6@gmail.com>
2023-04-23 23:37:30 -0700

    Add more optimal solution to problem

--- sortedSquaredArray.js
+++ sortedSquaredArray.js
@@ -1,4 +1,25 @@
+// O(nlog(n)) Time and O(n) Space Complexity
+// const sortedSquareArray = (array) => {
+//   let result = array.map((num) => num ** 2);
+//   return result.sort((a, b) => a - b);
+// };
+
+// O(n) Time and O(n) Space Complexity
 const sortedSquareArray = (array) => {
-  let result = array.map((num) => num ** 2);
-  return result.sort((a, b) => a - b);
+  let result = Array(array.length).fill(0);
+  let left = 0;
+  let right = array.length - 1;
+  let insert = right;
+  while (left <= right) {
+    if (array[left] ** 2 <= array[right] ** 2) {
+      result[insert] = array[right] ** 2;
+      right--;
+      insert--;
+      continue;
+    }
+    result[insert] = array[left] ** 2;
+    left++;
+    insert--;
+  }
+  return result;
 };
