d4038ba5fae16d8730cb8b911ab2d9027f1287f9
Sam Julian <samjulian6@gmail.com>
2023-05-26 21:49:31 -0700

    Add solution to selection sort problem

---
+++ selectionSort.js
@@ -0,0 +1,15 @@
+// O(n^2) Time and O(1) Space Complexity
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[minIndex] > array[j]) {
+        minIndex = j;
+      }
+    }
+    let temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+  }
+  return array;
+};
