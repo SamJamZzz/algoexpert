c278a1b63c288a7cb6d63e3db858e306765a8e5f
Sam Julian <samjulian6@gmail.com>
2023-06-14 19:45:07 -0700

    Add unoptimal solution to palindrome check problem

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,3 +1,18 @@
+// O(n) Time and O(n) Space Complexity
+// const isPalindrome = (string) => {
+//   let n = string.length;
+//   if (n <= 1) {
+//     return true;
+//   }
+//   if (n === 2) {
+//     return string[0] === string[1];
+//   }
+//   if (string[0] === string.slice(-1)) {
+//     return isPalindrome(string.slice(1, -1));
+//   }
+//   return false;
+// };
+
 // O(n) Time and O(1) Space Complexity
 const isPalindrome = (string) => {
   let left = 0;
